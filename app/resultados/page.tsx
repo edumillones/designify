1e18474a551e7dacfd4f5c73045bfcd19104cdeb
Eduardo <eduardo.millones.v@gmail.com>
2024-11-21 16:16:38 -0500

    Modificar pagina de inicio paquetes, dominio busqueda y arreglar singin dialog ademas de ajustar estilos en todas las pagina y agregar paginas Legaless

--- app/resultados/page.tsx
+++ app/resultados/page.tsx
@@ -1,4 +1,4 @@
-import ResultsPage from './Resultados/index'; // Ensure this path matches the directory structure
+import ResultsPage from './Resultado/index'; // Ensure this path matches the directory structure
 
 export default function Resultados() {
   return <ResultsPage />;
